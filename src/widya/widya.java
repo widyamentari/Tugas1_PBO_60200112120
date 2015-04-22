b0d0653a320cf4929fe9980e814e0d589b231489
60200112101@uin-alauddin.ac.id <60200112120@uin-alauddin.ac.id>
2015-04-22 18:48:01 +0800

    versi 1.0.0

---
+++ src/widya/widya.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package widya;
+
+/**
+ * @author acer
+ *
+ */
+public class widya {
+
+	/**
+	 * @param args
+	 */
+	static String laporan1 = "Print";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String laporan1 = "BIODATA";
+		System.out.println(laporan1);
+		biodata();
+		laporan1();
+
+	}
+	
+	public static void biodata()
+	{
+		System.out.println("widya mentari utami");
+		System.out.println("60200112120");
+		System.out.println("KelasC");
+	}
+	
+	public static void laporan1()
+	{
+		int x  = 15/3;
+		int y=3/8;
+		System.out.println("PEMBAGIAN");
+		System.out.println("Hasilnya Adalah:"+" "+x);
+		System.out.println("Hasilnya Adalah:"+" "+y);
+	}
+
+}
